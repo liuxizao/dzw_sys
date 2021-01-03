a8fe6fac30e760eb967986c212ea0b9f203a3c74
joker <1751085399@qq.com>
2021-01-03 20:06:09 +0800

    ljh

--- dzw_sys_dao/src/main/java/com/accp/dao/ljh/ljh_zhiweiMapper.java
+++ dzw_sys_dao/src/main/java/com/accp/dao/ljh/ljh_zhiweiMapper.java
@@ -17,8 +17,16 @@ public interface ljh_zhiweiMapper {
 	 * 查询职位的信息
 	 * @return
 	 */
-	@Select("select * from zhiwei where zname like '${zwname}%'")
+	@Select("select * from zhiwei where zname like '%${zwname}%'")
 	List<Zhiwei> selectZhiWeiAll(@Param("zwname")String zwname);
+
+	/**
+	 * 根据技工名称查询
+	 * @param starts
+	 * @return
+	 */
+	@Select("select * from zhiwei where zname=#{zname}")
+	Zhiwei selectByName(String zname);
 	
 	
 }
